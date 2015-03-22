6125d01ce18329bf9b74a59c29c37960f9da5518
wcy123 <wcy123@gmail.com>
2015-03-22 14:47:31 +0800

    prepare to export read_module_info to be one of builtin module.

--- voba.c
+++ voba.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dlfcn.h>
-#include <exec_once.h>
+#define EXEC_ONCE_TU_NAME "voba"
+#define EXEC_ONCE_DEPENDS {"voba.module"}
 #include <voba/value.h>
-#include <voba/module.h>
+#include <voba/core/sys.h>
+static voba_value_t get_argv(int argc, char * argv[])
+{
+    voba_value_t * p = (voba_value_t*)GC_MALLOC(sizeof(voba_value_t)*(argc+1));
+    assert(p);
+    p[0] = argc;
+    for(int i = 0; i < argc; i ++){
+        p[i+1] = voba_make_string(voba_str_from_cstr(argv[i]));
+    }
+    return voba_make_tuple(p);
+}
 int main(int argc, char *argv[])
 {
+    void *handle;
     exec_once_init();
-    voba_value_t symbols [] = {0};
-    voba_import_module(argv[1],"__main__",voba_make_tuple(symbols));
+    voba_symbol_set_value(s_argv,get_argv(argc,argv));
+    handle = dlopen(argv[1], RTLD_LAZY|RTLD_GLOBAL);
+    if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        exit(EXIT_FAILURE);
+    }
+    exec_once_init();
+    dlclose(handle);    
     exit(EXIT_SUCCESS);
 }
